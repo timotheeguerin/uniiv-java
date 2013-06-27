e5aeaae15c7600e82af36af88b25576575979778
Timothée Guérin <timothee.guerin@outlook.com>
2013-06-27 16:04:01 -0400

    efsawd

---
+++ Bendo/WebContent/scripts/googlemaps.js
@@ -0,0 +1,71 @@
+var marker;
+var geocoder;
+
+var maps = {};
+
+$(document).ready(function() {
+	$(".googlemap").each(function() {
+		initializeMap($(this));
+	});
+	
+	$(".googlemap.userMarker").each(function() {
+		initializeChooseLocationMap($(this));
+	});
+});
+function getCenter(element) {
+	var x = 0;
+	var y = 0;
+	var position = element.attr("data-map-position");
+	if (position != null) {
+		var array = position.split(",", 2);
+		x = array[0];
+		y = array[1];
+	}
+
+	return new google.maps.LatLng(x, y);
+}
+
+function initializeMap(element) {
+	var center = getCenter(element);
+	var map = new google.maps.Map(element[0], {
+		center : center,
+		zoom : 16,
+		mapTypeId : google.maps.MapTypeId.ROADMAP
+	});
+	
+	maps[element] = map;
+}
+function initializeChooseLocationMap(element) {
+	geocoder = new google.maps.Geocoder();
+	
+	google.maps.event.addListener(maps[element], 'click', function(event) {
+		placeMarker(maps[element], event.latLng);
+	});
+
+}
+
+function placeMarker(map, location) {
+	if (marker) {
+		marker.setPosition(location);
+	} else {
+		marker = new google.maps.Marker({
+			position : location,
+			map : map
+		});
+	}
+}
+
+function codeAddress() {
+	var address = document.getElementById('address').value;
+
+	geocoder.geocode({
+		'address' : address
+	}, function(results, status) {
+		if (status == google.maps.GeocoderStatus.OK) {
+			map.setCenter(results[0].geometry.location);
+			placeMarker(results[0].geometry.location);
+		} else {
+			alert('Geocode was not successful for the following reason: ' + status);
+		}
+	});
+}
