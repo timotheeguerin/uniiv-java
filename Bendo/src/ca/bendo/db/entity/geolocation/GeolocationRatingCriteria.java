23840f96d373bbd32388e10de4215b1875c9b3d5
Timothée Guérin <timothee.guerin@outlook.com>
2013-06-27 17:39:35 -0400

    Geolocation dao and table

---
+++ Bendo/src/ca/bendo/db/entity/geolocation/GeolocationRatingCriteria.java
@@ -0,0 +1,117 @@
+/**
+ * 
+ */
+package ca.bendo.db.entity.geolocation;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Formula;
+
+/**
+ * @author Timothée Guérin
+ * @version Bendo
+ * 
+ *          <b>geolocation</b>
+ *          <p>
+ *          </p>
+ * 
+ * 
+ */
+@Entity
+@Table(name = "geolocation_rating_criteria")
+public class GeolocationRatingCriteria
+{
+	/**
+	 * 
+	 */
+	@Id
+	@GeneratedValue
+	@Column(name = "id_geolocation_rating_criteria")
+	private long id;
+
+	/**
+	 * 
+	 */
+	@Column(name = "name")
+	private String name;
+
+	/**
+	 * 
+	 */
+	@Formula("(SELECT t.translation FROM lang_translation t "
+			+ "WHERE (t.key = name) AND t.id_lang_language = :languageId.param)")
+	private String translation;
+
+	/**
+	 * @return the id
+	 */
+	public long getId()
+	{
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(final long id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(final String name)
+	{
+		this.name = name;
+	}
+
+	/**
+	 * @return the translation
+	 */
+	public String getTranslation()
+	{
+		if (translation == null)
+		{
+			return name;
+		} else
+		{
+			return translation;
+		}
+	}
+
+	/**
+	 * @param translation
+	 *            the translation to set
+	 */
+	public void setTranslation(final String translation)
+	{
+		this.translation = translation;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		return getTranslation();
+	}
+
+}
---
+++ Bendo/src/ca/bendo/db/entity/geolocation/UserGeolocationRating.java
@@ -0,0 +1,125 @@
+/**
+ * 
+ */
+package ca.bendo.db.entity.geolocation;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import javax.persistence.Entity;
+
+/**
+ * @author Timothée Guérin
+ * @version Bendo
+ * 
+ *          <b>UserGeolocationRating</b>
+ *          <p>
+ *          </p>
+ * 
+ * 
+ */
+@Entity
+@Table(name = "user_geolocation_rating")
+public class UserGeolocationRating
+{
+	/**
+	 * 
+	 */
+	@Id
+	@GeneratedValue
+	@Column(name = "id_user_geolocation_rating")
+	private long id;
+
+	/**
+	 * 
+	 */
+	@ManyToOne
+	@JoinColumn(name = "id_user_geolocation_review")
+	private UserGeolocationReview review;
+
+	/**
+	 * 
+	 */
+	@ManyToOne
+	@JoinColumn(name = "id_geolocation_criteria")
+	private GeolocationRatingCriteria criteria;
+
+	/**
+	 * 
+	 */
+	@Column(name = "value")
+	private int value;
+
+	/**
+	 * @return the id
+	 */
+	public long getId()
+	{
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(final long id)
+	{
+		this.id = id;
+	}
+
+	/**
+	 * @return the review
+	 */
+	public UserGeolocationReview getReview()
+	{
+		return review;
+	}
+
+	/**
+	 * @param review
+	 *            the review to set
+	 */
+	public void setReview(final UserGeolocationReview review)
+	{
+		this.review = review;
+	}
+
+	/**
+	 * @return the criteria
+	 */
+	public GeolocationRatingCriteria getCriteria()
+	{
+		return criteria;
+	}
+
+	/**
+	 * @param criteria
+	 *            the criteria to set
+	 */
+	public void setCriteria(final GeolocationRatingCriteria criteria)
+	{
+		this.criteria = criteria;
+	}
+
+	/**
+	 * @return the value
+	 */
+	public int getValue()
+	{
+		return value;
+	}
+
+	/**
+	 * @param value
+	 *            the value to set
+	 */
+	public void setValue(final int value)
+	{
+		this.value = value;
+	}
+
+}
