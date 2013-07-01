7dd3d0ff3283f4bc97379877731d03dbd324a03c
Timothée Guérin <timothee.guerin@outlook.com>
2013-07-01 11:24:16 -0400

    jiuby

--- Bendo/src/ca/bendo/db/entity/user/UserBookmark.java
+++ Bendo/src/ca/bendo/db/entity/user/UserBookmark.java
@@ -8,9 +8,13 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import ca.bendo.db.entity.university.University;
 import ca.bendo.db.entity.wiki.Wiki;
@@ -32,7 +36,8 @@ public class UserBookmark
 	/**
 	 * 
 	 */
-	@ManyToMany(cascade = { CascadeType.ALL })
+	@ManyToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
+	@Fetch(FetchMode.SELECT)
 	@JoinTable(name = "user_bookmark_wiki", joinColumns = { @JoinColumn(name = "id_user") },
 			inverseJoinColumns = { @JoinColumn(name = "id_wiki") })
 	private List<Wiki> wikis = new ArrayList<Wiki>();
@@ -40,7 +45,8 @@ public class UserBookmark
 	/**
 	 * 
 	 */
-	@ManyToMany(cascade = { CascadeType.ALL })
+	@ManyToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
+	@Fetch(FetchMode.SELECT)
 	@JoinTable(name = "user_bookmark_university", joinColumns = { @JoinColumn(name = "id_user") },
 			inverseJoinColumns = { @JoinColumn(name = "id_uni_university") })
 	private List<University> unis = new ArrayList<University>();
